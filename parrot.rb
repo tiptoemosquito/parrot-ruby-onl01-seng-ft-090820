ab24e7ec3def1858080defb0a4e640b22bbc2333
Christina Howard <clhoward33@icloud.com>
2020-09-09 17:16:06 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrotphrase (name="Squawk!")
-  "Squawk!"
   puts "Squawk!"
   return "Squawk!"
-  
+  "Squawk!"
 end
